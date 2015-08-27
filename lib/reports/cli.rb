b97ca5bf92d91d63404e402d49bb4694a431c5e7
eduardopoleo <eduardopoleo@gmail.com>
2015-08-26 22:06:09 -0400

    error handling

--- lib/reports/cli.rb
+++ lib/reports/cli.rb
@@ -9,14 +9,19 @@ module Reports
 
   class CLI < Thor
     desc "user_info USERNAME", "Get information for a user"
-    def self.user_info(user_name)
+
+    def user_info(user_name)
       puts "Getting info for #{user_name}"
 
       client = GitHubAPIClient.new
       user = client.user_info(user_name)
+
       puts "name: #{user.name}"
       puts "location: #{user.location}"
       puts "public repos: #{user.public_repos}"
+    rescue NonExistentUser => error
+      puts "ERROR: #{error.message}"
+      exit 1
     end
 
     desc "console", "Open an RB session with all dependencies loaded and API defined."
--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -1,16 +1,26 @@
 require 'faraday'
 require 'json'
+require 'logger'
 
 module Reports
+  class NonExistentUser < StandardError; end
+  
   User = Struct.new(:name, :location, :public_repos)
   class GitHubAPIClient
-    def self.user_info(username)
+    def initialize
+      @logger = Logger.new(STDOUT)
+    end
+
+    def user_info(username)
       url = "https://api.github.com/users/#{username}"
       start_time = Time.now
       response = Faraday.get(url)
       duration = Time.now - start_time
 
-      puts '-> %s %s %d (%.3f s)' % [url, 'GET', response.status, duration]
+      @logger.debug '-> %s %s %d (%.3f s)' % [url, 'GET', response.status, duration]
+      if response.status == 404
+        raise NonExistentUser, "#{username} does not exist"
+      end
 
       data = JSON.parse(response.body)
       User.new(data["name"], data["location"], data["public_repos"])
