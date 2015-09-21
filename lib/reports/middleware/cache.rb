3db69086ef04f1417398d24a82c925015a933623
eduardopoleo <eduardopoleo@gmail.com>
2015-09-21 19:54:29 -0400

    Revert "Copy and paste of 2 lessons"
    This reverts commit 6090c4e0613bd9247a24e4ed245b7c98ad020bec.

--- lib/reports/middleware/cache.rb
+++ lib/reports/middleware/cache.rb
@@ -1,70 +1,53 @@
+require "time"
+
 module Reports
   module Middleware
     class Cache < Faraday::Middleware
-      def initialize(app, storage)
+      def initialize(app)
         super(app)
-        @app = app
-        @storage = storage
+        #Cache is in reality just a storage
+        @storage = {}
       end
 
       def call(env)
-        key = env.url.to_s
-        cached_response = @storage.read(key)
+        key = env.url
+        return @storage[key] if @storage[key]
+      # [status, headers, body]
+        @app.call(env).on_complete do
+           cache_response(env) if response_cachable?(env)
+        end
+      end
 
-        if cached_response
-          if fresh?(cached_response)
-            return @storage.read(key) if !needs_revalidation?(cached_response)
+      private
+
+      def cache_response(env)
+        key = env.url
+        @storage[key] = env.response
+      end
+
+      def response_cachable?(env)
+        env.method == :get && !needs_revalidation?(env)
+      end
+
+      def needs_revalidation?(env)
+        headers_prevent_caching?(env) || cache_is_stale?(env)
+      end
+
+      def headers_prevent_caching?(env)
+        headers = env.response_headers
+        cache_codes = ["no-cache", "no-store", "must-revalidate"]
+        headers.empty? || cache_codes.any? {|code| headers["Cache-Control"].include?(code)}
+      end
+
+      def cache_is_stale?(env)
+        headers = env.response_headers
+        if headers["Cache-Control"].include? "max-age"
+          period = headers["Cache-Control"][/\d+/].to_i
+          if (Time.now - Time.httpdate(headers["Date"])) > period
+            return true
           else
-            env.request_headers["If-None-Match"] = cached_response.headers['ETag']
+            return false
           end
-        end
-        response = @app.call(env)
-        response.on_complete do |response_env|
-          if cachable_response?(response_env)
-            if response.status == 304
-              cached_response = @storage.read(key)
-              cached_response.headers['Date'] = response.headers['Date']
-              @storage.write(key, cached_response)
-
-              response.env.update(cached_response.env)
-            else
-              @storage.write(key, response)
-            end
-          end
-        end
-        response
-      end
-
-      def cachable_response?(env)
-        env.method == :get && env.response_headers['Cache-Control'] && !env.response_headers['Cache-Control'].include?('no-store')
-      end
-
-      def needs_revalidation?(cached_response)
-        cached_response.headers['Cache-Control'] == 'no-cache' || cached_response.headers['Cache-Control'] == 'must-revalidate'
-      end
-
-      def fresh?(cached_response)
-        age = cached_response_age(cached_response)
-        max_age = cached_response_max_age(cached_response)
-
-        if age && max_age # Always stale without these values
-          age <= max_age
-        end
-      end
-
-      def cached_response_age(cached_response)
-        date = cached_response.headers['Date']
-        if date
-          time = Time.httpdate(date)
-          (Time.now - time).floor
-        end
-      end
-
-      def cached_response_max_age(cached_response)
-        cache_control = cached_response.headers['Cache-Control']
-        if cache_control
-          match = cache_control.match(/max\-age=(\d+)/)
-          match[1].to_i if match
         end
       end
     end
