c90ed3a79dc64897a20c734e8190b7f0d8447ea7
eduardopoleo <eduardopoleo@gmail.com>
2015-09-21 22:31:27 -0400

    Expire cache that exceeds max age

--- lib/reports/middleware/cache.rb
+++ lib/reports/middleware/cache.rb
@@ -3,31 +3,59 @@ module Reports
     class Cache < Faraday::Middleware
       def initialize(app)
         super(app)
-        @app = app
         @storage = {}
       end
 
       def call(env)
         key = env.url.to_s
         cached_response = @storage[key]
-        
-        if cached_response && !needs_revalidation?(cached_response)
+
+        if cached_response && !mandatory_refresh?(cached_response) && fresh?(cached_response) #checks for "must-revalidate" + "no-cache" + "stale"
           return cached_response
         end
 
         response = @app.call(env)
+        return response unless env.method == :get #This shit should be down to forbids_storage with a comment or smt
+
         response.on_complete do |response_env|
-          @storage[key] = response if cachable_response?(response_env)
+          cache_control_header = response_env.response_headers['Cache-Control']
+          if cache_control_header && !forbids_storage?(cache_control_header) #checks if no-store is present and there are cache parameters
+            @storage[key] = response
+          end
         end
         response
       end
 
-      def cachable_response?(env)
-        env.method == :get && env.response_headers['Cache-Control'] && !env.response_headers['Cache-Control'].include?('no-store')
+      private
+
+      def mandatory_refresh?(cached_response)
+        cached_response.headers['Cache-Control'].include?('no-cache') || cached_response.headers['Cache-Control'].include?('must-revalidate')
       end
 
-      def needs_revalidation?(cached_response)
-        cached_response.headers['Cache-Control'] == 'no-cache' || cached_response.headers['Cache-Control'] == 'must-revalidate'
+      def forbids_storage?(cache_control_header)
+        cache_control_header.include?('no-store')
+      end
+
+      def fresh?(cached_response)
+        age = response_age(cached_response)
+        max_age = response_max_age(cached_response)
+
+        if age && max_age # Always stale without these values
+          age <= max_age
+        end
+      end
+
+      def response_age(cached_response)
+        date = cached_response.headers['Date']
+        time = Time.httpdate(date) if date
+        (Time.now - time).floor if time
+      end
+
+      def response_max_age(cached_response)
+        cache_control = cached_response.headers['Cache-Control']
+        return nil unless cache_control
+        match = cache_control.match(/max\-age=(\d+)/)
+        match[1].to_i if match
       end
     end
   end
--- spec/reports/cache_spec.rb
+++ spec/reports/cache_spec.rb
@@ -1,4 +1,5 @@
 require "faraday"
+require "time"
 require "reports/middleware/cache"
 
 module Reports::Middleware
@@ -13,7 +14,7 @@ module Reports::Middleware
     end
 
     it "returns a previously cached response" do
-      stubs.get("http://example.test") { [200, {'Cache-Control' => 'public'}, "hello"] }
+      stubs.get("http://example.test") { [200, { 'Cache-Control' => 'public max-age=60', 'Date' => Time.now.httpdate }, "hello"] }
       conn.get("http://example.test")
       stubs.get("http://example.test") { [404, {}, "not found"] }
 
@@ -32,7 +33,7 @@ module Reports::Middleware
       end
     end
 
-   it "does not cache when the response doesn't have Cache-Control header" do
+    it "does not cache when the response doesn't have Cache-Controll header" do
       stubs.get("http://example.test") { [200, {}, "hello"] }
       conn.get("http://example.test")
       stubs.get("http://example.test") { [404, {}, "not found"] }
@@ -41,7 +42,7 @@ module Reports::Middleware
       expect(response.status).to eql(404)
     end
 
-    it "does not cache when the response Cache-Control header has no-store value" do
+    it "does not cache when the response Cache-Controll header has no-store value" do
       stubs.get("http://example.test") { [200, {'Cache-Control' => 'no-store'}, "hello"] }
       conn.get("http://example.test")
       stubs.get("http://example.test") { [404, {}, "not found"] }
@@ -50,7 +51,7 @@ module Reports::Middleware
       expect(response.status).to eql(404)
     end
 
-    it "does not use cached response when the response Cache-Control header has no-cache value" do
+    it "does not use cached response when the response Cache-Controll header has no-cache value" do
       stubs.get("http://example.test") { [200, {'Cache-Control' => 'no-store'}, "hello"] }
       conn.get("http://example.test")
       stubs.get("http://example.test") { [404, {}, "not found"] }
@@ -59,8 +60,27 @@ module Reports::Middleware
       expect(response.status).to eql(404)
     end
 
-    it "does not use cached response when the response Cache-Control header has must-revalidate value" do
+    it "does not use cached response when the response Cache-Controll header has must-revalidate value" do
       stubs.get("http://example.test") { [200, {'Cache-Control' => 'must-revalidate'}, "hello"] }
+      conn.get("http://example.test")
+      stubs.get("http://example.test") { [404, {}, "not found"] }
+
+      response = conn.get "http://example.test"
+      expect(response.status).to eql(404)
+    end
+
+    it "uses cached response when it doesn't exceeds max age" do
+      stubs.get("http://example.test") { [200, { 'Cache-Control' => 'max-age=60', 'Date' => Time.now.httpdate}, "hello"] }
+      conn.get("http://example.test")
+      stubs.get("http://example.test") { [404, {}, "not found"] }
+
+      response = conn.get "http://example.test"
+      expect(response.status).to eql(200)
+    end
+
+    it "does not use cached response when it does exceeds max age" do
+      stubs.get("http://example.test") { [200, { 'Cache-Control' => 'max-age=60', 'Date' => (Time.now - 2 * 60).httpdate }, "hello"] }
+
       conn.get("http://example.test")
       stubs.get("http://example.test") { [404, {}, "not found"] }
 
