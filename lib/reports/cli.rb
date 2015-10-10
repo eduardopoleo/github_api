57e41c1c6ee60959d32e4112147f8e2ef4396e88
eduardopoleo <eduardopoleo@gmail.com>
2015-10-10 14:14:13 -0400

    Add table for better display

--- lib/reports/cli.rb
+++ lib/reports/cli.rb
@@ -12,20 +12,20 @@ module Reports
   # - Rescues the Api client error
   # - Outputs the result to the user
   class CLI < Thor
-    desc "user_info USERNAME", "Get information for a user"
-
+    desc "activity USERNAME", "Gets info about events for an specific user"
     def activity(user_name)
+      puts "Fetching activity summary for #{user_name}"
+
       client = GitHubAPIClient.new()
       event_activities = client.activity(user_name)
 
-      event_activities.each do |ea|
-        puts "#{ea.type} - #{ea.repo}"
-      end
+      print_activity_report(event_activities)
     rescue Error => error
       puts "ERROR #{error.message}"
       exit 1
     end
 
+    desc "user_info USERNAME", "Get information for a user"
     def user_info(user_name)
       puts "Getting info for #{user_name}"
       # Using dotenv The enviroment variables are stored in the .env which is
@@ -70,6 +70,22 @@ module Reports
       require 'irb'
       ARGV.clear
       IRB.start
+    end
+    private
+
+    def print_activity_report(events)
+      table_printer = TablePrinter.new(STDOUT)
+      event_types_map = events.each_with_object(Hash.new(0)) do |event, counts|
+        counts[event.type] += 1
+      end
+      #{pushevents: 2, commitevents: 5, commentevent: 9} etc, and this is require cus?
+      table_printer.print(event_types_map, title: "Event Summary", total: true)
+
+      push_events_map = events.each_with_object(Hash.new(0)) do |event, counts|
+        counts[event.repo] += 1
+      end
+      puts
+      table_printer.print(push_events_map, title: "Project Push Sumary", total: true)
     end
   end
 end
